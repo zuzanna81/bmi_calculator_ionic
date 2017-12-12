857025122e05791b09caa3a123207601a7d14e4a
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-12-12 11:21:21 +0100

    Add 2 extra variables to the calculator.ts file, bmiValue and bmiMessage Add implementation of the calculateBMI function to the calculator.ts file Add private method for setBMIMessage, calculator.ts file

--- src/pages/calculator/calculator.ts
+++ src/pages/calculator/calculator.ts
@@ -16,11 +16,35 @@ import { IonicPage } from 'ionic-angular';
 export class CalculatorPage {
   height: number;
   weight: number;
+  bmiValue: number;
+  bmiMessage: number;
 
   constructor() {
   }
 
   calculateBMI() {
+    if (this.weight > 0 && this.height > 0) {
+      let finalBmi = this.weight / (this.height / 100 * this.height / 100);
+      this.bmiValue = parseFloat(finalBmi.toFixed(2));
+      this.setBMIMessage();
+    }
+  }
 
+  private setBMIMessage() {
+    if (this.bmiValue < 18.5) {
+      this.bmiMessage = "Underweight"
+    }
+
+    if (this.bmiValue > 18.5 && this.bmiValue < 25) {
+      this.bmiMessage = "Normal"
+    }
+
+    if (this.bmiValue > 25 && this.bmiValue < 30) {
+      thisbmiMessage = "Overweight"
+    }
+
+    if (this.bmiValue > 30) {
+      this.bmiMessage = "Obese"
+    }
   }
 }
